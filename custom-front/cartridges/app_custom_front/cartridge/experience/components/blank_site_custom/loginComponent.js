1cb152b8a95ae79afcc80c6a94e31486bed2d5f3
Lior Fridman <76394602+liorspidy@users.noreply.github.com>
2023-02-15 14:13:48 +0200

    fixed custom page designer components

--- custom-front/cartridges/app_custom_front/cartridge/experience/components/blank_site_custom/loginComponent.js
+++ custom-front/cartridges/app_custom_front/cartridge/experience/components/blank_site_custom/loginComponent.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
-const Template = require('dw/util/Template');
-const HashMap = require('dw/util/HashMap');
-const URLUtils = require('dw/web/URLUtils');
+const Template = require("dw/util/Template");
+const HashMap = require("dw/util/HashMap");
+const URLUtils = require("dw/web/URLUtils");
 
 /**
  * Render logic for storefront.productBannerStrip component.
@@ -15,7 +15,11 @@ module.exports.render = function (context) {
   const content = context.content;
 
   model.BtnPosition = content.BtnPosition;
+  model.backgroundColor = content.backgroundColor.value
+    ? content.backgroundColor.value
+    : "#ffffff";
+
   return new Template(
-    'experience/components/blank_site_custom/loginComponent'
+    "experience/components/blank_site_custom/loginComponent"
   ).render(model).text;
 };
--- custom-front/cartridges/app_custom_front/cartridge/experience/components/blank_site_custom/searchComp.js
+++ custom-front/cartridges/app_custom_front/cartridge/experience/components/blank_site_custom/searchComp.js
@@ -15,7 +15,40 @@ module.exports.render = function (context) {
   const content = context.content;
 
   model.leftOrRight = content.leftOrRight;
-  model.cartColor.value = content.cartNumColor.value;
+  model.cartColor = content.cartNumColor.value;
+  model.backgroundColor = content.backgroundColor.value
+    ? content.backgroundColor.value
+    : "#ffffff";
+
+  let r = 0,
+    g = 0,
+    b = 0; // Check if hex code is valid
+  // Convert hex code to RGB parameters
+  const hexToRgb = (hex) => {
+    if (hex.match(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/)) {
+      // If hex code is in 6-digit format, grab the 3 pairs of characters
+      if (hex.length === 7) {
+        r = parseInt(hex.substring(1, 3), 16);
+        g = parseInt(hex.substring(3, 5), 16);
+        b = parseInt(hex.substring(5, 7), 16);
+      } else {
+        // If hex code is in 3-digit format, repeat each character twice
+        r = parseInt(hex[1] + hex[1], 16);
+        g = parseInt(hex[2] + hex[2], 16);
+        b = parseInt(hex[3] + hex[3], 16);
+      }
+    }
+
+    return [r, g, b];
+  };
+
+  model.bgred = hexToRgb(model.backgroundColor)[0];
+  model.bggreen = hexToRgb(model.backgroundColor)[1];
+  model.bgblue = hexToRgb(model.backgroundColor)[2];
+
+  model.cartred = hexToRgb(model.cartColor)[0];
+  model.cartgreen = hexToRgb(model.cartColor)[1];
+  model.cartblue = hexToRgb(model.cartColor)[2];
 
   return new Template(
     "experience/components/blank_site_custom/searchComp"
